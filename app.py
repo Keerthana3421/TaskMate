9975ab306a156e458d26088e8899e71a1397d8c2
Keerthana3421 <keerthanagudipalli033@gmail.com>
2025-04-12 15:24:27 -0500

    Add TODO for future email reminder feature

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# TODO: Add email reminders for upcoming due tasks (future feature)
+
 app = Flask(__name__)
 app.secret_key = 'taskmate_secret_key'
 
